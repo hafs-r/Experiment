c437a0f7418e9af02824b9813f3c56a861b733d1
hafsalrahman <hafsalrahman13@gmail.com>
2017-10-25 00:10:54 +0400

    added basic mvp interfaces

---
+++ app/src/main/java/com/hafsalrahman/ssstest/base/BasePresenter.java
@@ -0,0 +1,10 @@
+package com.hafsalrahman.ssstest.base;
+
+/**
+ * Created by hafsal on 10/24/17.
+ */
+
+
+public interface BasePresenter {
+    void unsubscribe();
+}
---
+++ app/src/main/java/com/hafsalrahman/ssstest/base/BaseView.java
@@ -0,0 +1,10 @@
+package com.hafsalrahman.ssstest.base;
+
+
+/**
+ * Created by hafsal on 10/24/17.
+ */
+
+public interface BaseView {
+    void setPresenter(BasePresenter presenter);
+}
